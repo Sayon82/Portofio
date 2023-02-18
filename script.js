b4aa13c818293e0ecf04af0571ca31945758d2a2
Sayon82 <117695653+Sayon82@users.noreply.github.com>
2023-02-18 12:20:27 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+let menu = document.querySelector('#btr');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
